791db88e055cf210c84416f5882bb0855c370db8
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2024-05-23 23:46:29 +0700

    fix: footer sticks to bottom with short content

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ const App: React.FC = () => {
   } = theme.useToken();
 
   return (
-    <Layout hasSider>
+    <Layout hasSider style={{ minHeight: "100vh" }}>
       <Sider
         collapsible
         collapsed={collapsed}
@@ -102,7 +102,6 @@ const App: React.FC = () => {
         />
       </Sider>
       <Layout style={{ marginLeft: 256 }}>
-        {/*<Header style={{ padding: 0, background: colorBgContainer }} />*/}
         <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
           <div
             style={{
@@ -112,19 +111,12 @@ const App: React.FC = () => {
               borderRadius: borderRadiusLG,
             }}
           >
-            <p>long content</p>
-            {
-              // indicates very long content
-              Array.from({ length: 100 }, (_, index) => (
-                <React.Fragment key={index}>
-                  {index % 20 === 0 && index ? "more" : "..."}
-                  <br />
-                </React.Fragment>
-              ))
-            }
+            <p>Insert paragraph here</p>
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
+        <Footer
+          style={{ textAlign: "center", position: "sticky", bottom: "0" }}
+        >
           Ant Design ©{new Date().getFullYear()} Created by Ant UED
         </Footer>
       </Layout>
