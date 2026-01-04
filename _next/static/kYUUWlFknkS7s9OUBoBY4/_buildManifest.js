b4a51d42d473e4c613bdbb4b3f20e47d6be5b5a5
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2026-01-05 00:16:20 +0700

    Updates

---
+++ _next/static/kYUUWlFknkS7s9OUBoBY4/_buildManifest.js
@@ -0,0 +1,35 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/4cd5590af9f3c248.js"
+  ],
+  "/_error": [
+    "static/chunks/53c12b9e0be0d463.js"
+  ],
+  "/about": [
+    "static/chunks/424719af9e2d5bef.js"
+  ],
+  "/experiences": [
+    "static/chunks/e437484775aa3ae6.js"
+  ],
+  "/personal-projects": [
+    "static/chunks/f23b2c4d5dd467a9.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/public/_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/about",
+    "/experiences",
+    "/personal-projects"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
